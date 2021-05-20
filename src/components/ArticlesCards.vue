4bcd501ad700dacbd3c615c927d9152451f055aa
jose.nuvensis <arniel.pama@nuvensis.ch>
2021-05-20 10:16:50 +0800

    Add transparent background per article area or section

--- src/components/ArticlesCards.vue
+++ src/components/ArticlesCards.vue
@@ -5,7 +5,7 @@
       <div
         v-for="i in 4"
         :key="'article-card' + i"
-        class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60"
+        class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60 section-background"
       >
         <h2 class="text-lg sm:text-xl font-medium title-font mb-2">Shooting Stars</h2>
         <p class="leading-relaxed text-base mb-4">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
