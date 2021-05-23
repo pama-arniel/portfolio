0bab889f5856f576c44a8d2d8fdf4105a8f8ac91
jose.nuvensis <arniel.pama@nuvensis.ch>
2021-05-23 16:17:50 +0800

    Add constants for emailjs credentials

---
+++ src/assets/email_config.js
@@ -0,0 +1,9 @@
+const SERVICE_ID = 'service_l1fc0ol';
+const TEMPLATE_ID =  'template_se5y6cc';
+const USER_ID = 'user_lCwch6ggdpc9rcBb3nUK9';
+
+export default {
+    SERVICE_ID: SERVICE_ID,
+    TEMPLATE_ID: TEMPLATE_ID,
+    USER_ID: USER_ID
+}
