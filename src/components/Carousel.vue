44464c7f72a5494d98c442bb1684b65efe4655a6
Jose Arniel Pama <pamajosearniel@gmail.com>
2021-05-29 04:37:04 +0800

    Add new style for search bar component

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,34 +1,24 @@
 <template>
 <div>
-   <div class="flex flex-wrap justify-center items-center mb-5">
-      <!-- the search bar -->
-      <div class="relative">
-         <input
-            v-model.trim="searchKey" type="text" class="h-12 w-70 sm:w-1/2 pr-8 pl-5 rounded z-0 focus:shadow focus:outline-none text-base text-black"
-            placeholder="Search anything..."
-            >
-         <div class="absolute top-4 right-3">
-            <svg version="1.1" class="h-4 text-dark" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-               viewBox="0 0 52.966 52.966" style="enable-background:new 0 0 52.966 52.966;" xml:space="preserve">
-               <path d="M51.704,51.273L36.845,35.82c3.79-3.801,6.138-9.041,6.138-14.82c0-11.58-9.42-21-21-21s-21,9.42-21,21s9.42,21,21,21
-                  c5.083,0,9.748-1.817,13.384-4.832l14.895,15.491c0.196,0.205,0.458,0.307,0.721,0.307c0.25,0,0.499-0.093,0.693-0.279
-                  C52.074,52.304,52.086,51.671,51.704,51.273z M21.983,40c-10.477,0-19-8.523-19-19s8.523-19,19-19s19,8.523,19,19
-                  S32.459,40,21.983,40z"/>
-            </svg>
-         </div>
-      </div>
-
-      <!-- tag pills -->
-      <div class="align-bottom ml-2">
-          <span
-            v-for="i in 3"
-            :key="i"
-            class="px-4 py-2 m-1 text-base rounded-full text-indigo-500 border border-indigo-500 undefined "
+  <div class="flex items-center justify-center pb-6">
+    <div class="box-wrapper text-base">
+        <div class="rounded-lg flex items-center w-full p-3 shadow-sm bg-white bg-opacity-80 border border-gray-300">
+          <button class="outline-none focus:outline-none"><svg class="w-5 text-gray-600 h-5 cursor-pointer" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg></button>
+          <input
+            v-model.trim="searchKey" type="search" placeholder="search for terms" x-model="q"
+            class="w-full pl-4 text-sm outline-none focus:outline-none bg-transparent text-black"
           >
-              Hello
-          </span>
-      </div>
-   </div>
+          <div class="select text-black">
+            <select x-model="image_type" class="text-sm outline-none focus:outline-none bg-transparent">
+              <option value="all" selected>All</option>
+              <option value="photo">Photo</option>
+              <option value="illustration">Illustration</option>
+              <option value="vector">Vector</option>
+              </select>
+          </div>
+        </div>
+    </div>
+  </div>
 
    <!-- Slideshow container -->
    <div class="slideshow-container">
