302497971c7f4d67fd175d3eb0209462008b15f0
Jose Arniel Pama <pamajosearniel@gmail.com>
2021-05-31 01:19:28 +0800

    Add configs for deployment to github pages

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+echo Linting..
+yarn lint
+echo Building. this may take a minute...
+yarn build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'example.com' > CNAME
+
+echo Deploying..
+git init
+git add -A
+git commit -m 'Deploy portfolio'
+
+# deploy
+git push -f git@github.com:pama-arniel/portfolio.git master:gh-pages
+
+cd -
