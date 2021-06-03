c83309da6fd9c3620f8f956553619e7f5a7e8da6
Jose Arniel Pama <pamajosearniel@gmail.com>
2021-06-04 03:01:40 +0800

    Add small note to click the card to see full details

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -2,7 +2,8 @@
 <section id="projects-section" class="text-white body-font">
   <div class="container px-5 sm:px-10 py-24 mx-auto">
     <div class="flex flex-col text-center w-full mb-6">
-      <h1 class="text-4xl title-font purple-header">Here Are Some of My Projects</h1>
+      <h1 class="text-4xl title-font purple-header mb-4">Here Are Some of My Projects</h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Click the cards to see the full details.</p>
     </div>
     <Carousel :refString="'projects'"/>
   </div>
